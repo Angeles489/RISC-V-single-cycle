4b4a2c8d2d04d4bead1e077e88373a364e7d4fab
Angeles Araiza <angelesaragar@hotmail.com>
2026-05-29 22:43:13 -0600

    Update instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,26 +1,15 @@
 module instruction_memory(
-	input [31:0] PC,
-	
-	output reg [31:0] Instr
-
+    input [31:0] A,
+    output [31:0] RD
 );
+    reg [31:0] instr_mem [0:2];  // solo tres instrucciones en InstrMem
 
-reg [31:0] Memory [0:255];
+    initial begin
+        $readmemh("instrMem.hex", instr_mem);
+        $display("instr_mem[0] = %h", instr_mem[0]);
+        $display("instr_mem[1] = %h", instr_mem[1]);
+        $display("instr_mem[2] = %h", instr_mem[2]);
+    end
 
-
-initial 
-begin
-	$readmemh("instrMem.hex", Memory);
-end
-
-
-always @(*)
-begin 
-	Instr = Memory[PC>>2];
-	
-end 
-
-
-
-
+    assign RD = instr_mem[A[31:2]];
 endmodule
