364bd129e65615bba705c6024186f2866ca21bd9
Angeles Araiza <angelesaragar@hotmail.com>
2026-05-29 22:43:59 -0600

    Refactor register_file to use assign for outputs
    Refactored register_file module to use continuous assignments for RD1 and RD2.

--- register_file.v
+++ register_file.v
@@ -1,30 +1,24 @@
 module register_file(
-	input clk,
-	input [4:0] A1,
-	input [4:0] A2,
-	input [4:0] A3,
-	input [31:0] WD3,
-	input WE3,
-	
-	output reg [31:0] RD1,
-	output reg [31:0] RD2
+    input clk,
+    input [4:0] A1,
+    input [4:0] A2,
+    input [4:0] A3,
+    input [31:0] WD3,
+    input WE3,
+    
+    output [31:0] RD1,
+    output [31:0] RD2
 );
-
-//REGISTRO TEMPORAL
+    
 reg [31:0] REG [31:0];
 
+assign RD1 = REG[A1];
+assign RD2 = REG[A2];
 
-always @(*)
-begin 
-	RD1= REG[A1];
-	RD2= REG[A2];
-end 
-
-
-always @(posedge clk)
-begin	
-	if(WE3==1)
-		REG[A3] <= WD3; 
+always @(posedge clk) 
+begin
+   if(WE3 == 1)
+		REG[A3] <= WD3;
 end
 
 endmodule
