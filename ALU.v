3a64efcfbca491fb9625bfa8f0e2334bfb3e0941
Angeles Araiza <angelesaragar@hotmail.com>
2026-05-29 22:41:43 -0600

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -12,11 +12,11 @@ module ALU(
 always @(*)
 begin 
 	case(ALUControl)
-	3'b000 : Result = A + B;
-	3'b001 : Result = A - B;
-	3'b010 : Result = A & B;
-	3'b011 : Result = A | B;
-	3'b101 : Result = A << B;
+	3'b000 : Result = A + B; //add
+	3'b001 : Result = A - B; //sub
+	3'b010 : Result = A & B; //and
+	3'b011 : Result = A | B; //or 
+	3'b101 : Result = A << B[4:0]; //shift
 	default: Result = 0;
 	endcase 
 	
