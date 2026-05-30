dcd74a26b81ca8067a10786a867a5509282746b4
Angeles Araiza <angelesaragar@hotmail.com>
2026-05-29 22:46:51 -0600

    Refactor ALU control logic with case statements
    Refactor ALU control logic to use case statements for ALUOp and funct3, improving clarity and functionality.

--- ALU_control.v
+++ ALU_control.v
@@ -2,12 +2,41 @@ module ALU_control(
 	input [1:0] ALUOp,
 	input [2:0] funct3,
 	input [6:0] funct7,
-	input Op,
 	
 	output reg [2:0] ALUControl
 );
 
-// Decidir que operación vamos a realizar
+
+always @(*)
+begin
+	case(ALUOp)
+		2'b00: ALUControl = 3'b000; //lw y sw
+		2'b01: ALUControl = 3'b001; //beq
+		2'b10: 
+			begin
+				case(funct3)
+					3'b000: 
+					begin
+						if(funct7 == 7'b0100000)
+							ALUControl = 3'b001; //sub
+						else
+							ALUControl = 3'b000; //add
+						end
+					
+					3'b111: ALUControl = 3'b010; //and
+					3'b110: ALUControl = 3'b011; //or
+					3'b010: ALUControl = 3'b101; //slt
+				
+				endcase
+			end
+			
+			default: ALUControl = 3'b000;
+			
+	 endcase
+end
+
+
+/*
 always @(*)
 begin
 	if(ALUOp == 00)begin
@@ -20,21 +49,22 @@ begin
 		begin
 		case(funct3)
 			3'b000 : begin
-						if({Op,funct7} == 00 | 01 |10)
-							ALUControl = 000;
-						else if({Op,funct7} == 11 )
-							ALUControl = 001;
+						//if({Op,funct7} == 00 | 01 |100)
+						if(funct7 == 7'b0100000)
+							ALUControl = 3'b001;
+						//else if({Op,funct7} == 11 )
+							ALUControl = 3'b000;
 						end
-			3'b010 : ALUControl = 101;
-			3'b110 : ALUControl = 011;
-			3'b111 : ALUControl = 010;
+			3'b010 : ALUControl = 3'b101;
+			3'b110 : ALUControl = 3'b011;
+			3'b111 : ALUControl = 3'b010;
 	
 			default : ALUControl = 0;			
 		endcase 
 	end
 			
 end
-
+*/
 
 
 endmodule
